1f590c57ed46096773db5cdc2c353d85db325884
rmens <r.mens@me.com>
2021-05-24 23:54:29 +0200

    Give GPOs time to finish

--- start.bat
+++ start.bat
@@ -32,10 +32,14 @@ curl -o C:\Windows\deploy\syspol.txt https://raw.githubusercontent.com/oszuidwes
 curl -o C:\Windows\deploy\firefoxpol.txt https://raw.githubusercontent.com/oszuidwest/windows10-baseline/master/gpo/firefoxpol.txt
 curl -o C:\Windows\deploy\edgepol.txt https://raw.githubusercontent.com/oszuidwest/windows10-baseline/master/gpo/edgepol.txt 
 
+TIMEOUT 5
+
 START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\userpol.txt /w C:\Windows\deploy\userpol.pol
 START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\syspol.txt /w C:\Windows\deploy\syspol.pol
 START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\firefoxpol.txt /w C:\Windows\deploy\firefoxpol.pol
 START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\edgepol.txt /w C:\Windows\deploy\edgepol.pol
+
+TIMEOUT 5
 
 START C:\Windows\deploy\LGPO.exe /un C:\Windows\deploy\userpol.pol
 START C:\Windows\deploy\LGPO.exe /m C:\Windows\deploy\syspol.pol
