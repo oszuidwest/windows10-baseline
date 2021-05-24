9431f5d6a8e2bc3f4f04168e5af44c4fbd6c9b1f
rmens <r.mens@me.com>
2021-05-25 00:12:55 +0200

    Move GPO section to own script

--- start.bat
+++ start.bat
@@ -25,26 +25,9 @@ CALL sudo.bat C:\Windows\deploy\power.bat
 :: Download and apply GPO baseline settings
 ::===============================================================
 
-curl -o C:\Windows\deploy\LGPO.exe https://raw.githubusercontent.com/oszuidwest/windows10-baseline/master/bin/LGPO.exe
+curl -o C:\Windows\deploy\gpo.bat https://raw.githubusercontent.com/oszuidwest/windows10-baseline/master/scripts/gpo.bat 
 
-curl -o C:\Windows\deploy\userpol.txt https://raw.githubusercontent.com/oszuidwest/windows10-baseline/master/gpo/userpol.txt
-curl -o C:\Windows\deploy\syspol.txt https://raw.githubusercontent.com/oszuidwest/windows10-baseline/master/gpo/syspol.txt 
-curl -o C:\Windows\deploy\firefoxpol.txt https://raw.githubusercontent.com/oszuidwest/windows10-baseline/master/gpo/firefoxpol.txt
-curl -o C:\Windows\deploy\edgepol.txt https://raw.githubusercontent.com/oszuidwest/windows10-baseline/master/gpo/edgepol.txt 
-
-TIMEOUT 5
-
-START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\userpol.txt /w C:\Windows\deploy\userpol.pol
-START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\syspol.txt /w C:\Windows\deploy\syspol.pol
-START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\firefoxpol.txt /w C:\Windows\deploy\firefoxpol.pol
-START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\edgepol.txt /w C:\Windows\deploy\edgepol.pol
-
-TIMEOUT 5
-
-START C:\Windows\deploy\LGPO.exe /un C:\Windows\deploy\userpol.pol
-START C:\Windows\deploy\LGPO.exe /m C:\Windows\deploy\syspol.pol
-START C:\Windows\deploy\LGPO.exe /m C:\Windows\deploy\firefoxpol.pol
-START C:\Windows\deploy\LGPO.exe /m C:\Windows\deploy\edgepol.pol
+CALL sudo.bat C:\Windows\deploy\gpo.bat
 
 ::===============================================================
 :: Instal package manager
