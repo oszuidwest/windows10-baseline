e1c2a0692b1b7db079671083d24e3b60515f8181
rmens <r.mens@me.com>
2021-05-24 23:15:36 +0200

    Add baseline apps

---
+++ scripts/apps.bat
@@ -0,0 +1,3 @@
+choco install firefox -y
+choco install vlc -y
+choco install teamviewer.host -y
--- start.bat
+++ start.bat
@@ -40,7 +40,15 @@ START C:\Windows\deploy\LGPO.exe /r C:\Windows\deploy\edgepol.txt /w C:\Windows\
 ::===============================================================
 :: Instal package manager
 ::===============================================================
+
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+::===============================================================
+:: Instal baseline apps
+::===============================================================
+
+curl -o C:\Windows\deploy\apps.bat https://raw.githubusercontent.com/oszuidwest/windows10-baseline/master/scripts/apps.bat
+CALL sudo.bat C:\Windows\deploy\apps.bat
 
 ::===============================================================
 :: Debloat Windows 10
