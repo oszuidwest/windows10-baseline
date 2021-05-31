e687e4318c563ce766d486adac345db274c1de8a
Raymon Mens <r.mens@me.com>
2021-05-31 23:51:42 +0200

    Add 7zip to baseline apps

--- scripts/apps.bat
+++ scripts/apps.bat
@@ -2,6 +2,7 @@
 if not "%_deploy_computer_purpose%"=="plain" (
     choco install firefoxesr -y
     choco install vlc -y
+    choho install 7zip -y
     choco install teamviewer.host -y --ignore-checksums
 ) else (
     echo plain configuration selected, not installing apps )
