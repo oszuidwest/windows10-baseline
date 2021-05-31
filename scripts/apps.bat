7d28b2087e2c65a4decb3d3f645c5016d5dff68f
Raymon Mens <r.mens@me.com>
2021-05-31 23:19:36 +0200

    Only install apps if purpose is not plain

--- scripts/apps.bat
+++ scripts/apps.bat
@@ -1,3 +1,7 @@
-choco install firefoxesr -y
-choco install vlc -y
-choco install teamviewer.host -y --ignore-checksums
+@echo off
+if not "_deploy_computer_purpose%=="plain" (
+    choco install firefoxesr -y
+    choco install vlc -y
+    choco install teamviewer.host -y --ignore-checksums
+) else (
+    echo plain configuration selected, not installing apps )
