709bc3ae9f1e734fbbefcb127acbd6b89f09a01c
Raymon Mens <r.mens@me.com>
2021-05-31 22:46:37 +0200

    Tweak power settings for laptops (not plugged in)

--- scripts/powerconf.bat
+++ scripts/powerconf.bat
@@ -1,9 +1,9 @@
 powercfg /change monitor-timeout-ac 30
 powercfg /change monitor-timeout-dc 30
 powercfg /change disk-timeout-ac 0
-powercfg /change disk-timeout-dc 0
+powercfg /change disk-timeout-dc 45
 powercfg /change standby-timeout-ac 0
-powercfg /change standby-timeout-dc 0
+powercfg /change standby-timeout-dc 60
 powercfg /change hibernate-timeout-ac 0
 powercfg /change hibernate-timeout-dc 0
 powercfg /hibernate off
