e1c1515d6379cc8dbdc0c5030481d0127ccc0234
Raymon Mens <r.mens@me.com>
2021-05-31 23:21:12 +0200

    Fix sloppy type job in apps.bat

--- scripts/apps.bat
+++ scripts/apps.bat
@@ -1,5 +1,5 @@
 @echo off
-if not "_deploy_computer_purpose%=="plain" (
+if not "%_deploy_computer_purpose%"=="plain" (
     choco install firefoxesr -y
     choco install vlc -y
     choco install teamviewer.host -y --ignore-checksums
