751bb6453b6d2c028e592657f2318107500f8e2a
Raymon Mens <r.mens@me.com>
2021-06-29 17:38:07 +0200

    Add purposes to apps script

--- scripts/apps.bat
+++ scripts/apps.bat
@@ -3,5 +3,10 @@ if not "%_deploy_computer_purpose%"=="plain" (
     choco install firefoxesr -y
     choco install vlc -y
     choco install 7zip -y
+    choco install libreoffice-fresh -y
     choco install teamviewer.host -y --ignore-checksums
+)
+if "%_deploy_computer_purpose%"=="fm" if "%_deploy_computer_purpose%"=="laptop" "%_deploy_computer_purpose%"=="redactie" (
+    choco install audacity -y
+    choco install paint.net -y
 )
