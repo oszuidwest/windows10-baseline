90901f90654e65d23307a4c3e0c8c29af88ee673
Raymon Mens <r.mens@me.com>
2022-06-26 20:43:30 +0200

    Don't install Firefox, Edge is good now

--- scripts/apps.bat
+++ scripts/apps.bat
@@ -1,6 +1,5 @@
 @echo off
 if not "%_deploy_computer_purpose%"=="plain" (
-    choco install firefoxesr -y
     choco install vlc -y
     choco install 7zip -y
     choco install libreoffice-fresh -y
