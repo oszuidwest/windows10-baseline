f831bf55a63207debbc5ed6efabbbb54ee0eea6a
rmens <r.mens@me.com>
2022-07-31 13:06:39 +0200

    Remove teamviewer

--- scripts/apps.bat
+++ scripts/apps.bat
@@ -3,9 +3,7 @@ if not "%_deploy_computer_purpose%"=="plain" (
     choco install vlc -y
     choco install 7zip -y
     choco install libreoffice-fresh -y
-    choco install teamviewer.host -y
     choco install microsoft-teams.install -y --params "'/AllUsers /NoAutoStart'"
-    del "%Public%\Desktop\TeamViewer*.lnk"
 )
 
 if "%_deploy_computer_purpose%"=="fm" (
