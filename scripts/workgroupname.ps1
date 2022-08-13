c8bb8631cd00f99b71257130f8fe8186fe334a98
Raymon Mens <r.mens@me.com>
2022-08-14 00:51:21 +0200

    Only change computer name if needed

--- scripts/workgroupname.ps1
+++ scripts/workgroupname.ps1
@@ -1,3 +1,7 @@
 $regname = Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\' -Name _deploy_computer_name
-Add-Computer -WorkGroupName oszw
+$curname = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+if ( $curname -ne $regname )
+{
 Rename-Computer -NewName "$regname" -Force
+}
+Add-Computer -WorkGroupName oszw
