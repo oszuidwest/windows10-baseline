a98ff0cbc0a3a328710582137d2273cb9053a58f
Raymon Mens <r.mens@me.com>
2022-08-14 00:53:42 +0200

    Check on workgroup too

--- scripts/workgroupname.ps1
+++ scripts/workgroupname.ps1
@@ -1,7 +1,11 @@
 $regname = Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\' -Name _deploy_computer_name
 $curname = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+$curgroup = (Get-CimInstance -ClassName Win32_ComputerSystem).Domain
 if ( $curname -ne $regname )
 {
 Rename-Computer -NewName "$regname" -Force
 }
+if ( $curgroup -ne oszw )
+{
 Add-Computer -WorkGroupName oszw
+}
