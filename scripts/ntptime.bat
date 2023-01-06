8632cdc08414d5b3ea25f310b0cdd48ba2fe9a31
Raymon Mens <r.mens@me.com>
2023-01-07 00:23:40 +0100

    Auto

--- scripts/ntptime.bat
+++ scripts/ntptime.bat
@@ -3,9 +3,8 @@ w32tm /unregister
 w32tm /unregister
 w32tm /register
 net start w32time
-w32tm /config /manualpeerlist:"0.nl.pool.ntp.org 1.nl.pool.ntp.org 2.nl.pool.ntp.org 3.nl.pool.ntp.org" /syncfromflags:manual /reliable:no /update
+w32tm /config /manualpeerlist:"0.nl.pool.ntp.org 1.nl.pool.ntp.org 2.nl.pool.ntp.org 3.nl.pool.ntp.org" /syncfromflags:manual /reliable:no /update /specialpollinterval:1440
 w32tm /config /update
 w32tm /resync /rediscover
 sc triggerinfo w32time delete
 sc config w32time start=auto
-# Todo: sync time every 3600 secs
