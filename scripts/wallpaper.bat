933d3a23418ce74b76f0cb9e9b0d71c00996f40d
Raymon Mens <r.mens@me.com>
2023-04-24 00:15:07 +0200

    Refactor wallpaper.bat

--- scripts/wallpaper.bat
+++ scripts/wallpaper.bat
@@ -1,4 +1,13 @@
 @echo off
-if "%_deploy_computer_purpose%"=="fm" curl -o C:\Windows\deploy\wallpaper.png https://raw.githubusercontent.com/oszuidwest/windows10-baseline/main/assets/ZWFM-wallpaper.png
-if "%_deploy_computer_purpose%"=="edit" curl -o C:\Windows\deploy\wallpaper.png https://raw.githubusercontent.com/oszuidwest/windows10-baseline/main/assets/ZWTV-wallpaper.png
-if "%_deploy_computer_purpose%"=="redactie" curl -o C:\Windows\deploy\wallpaper.png https://raw.githubusercontent.com/oszuidwest/windows10-baseline/main/assets/ZWUP-wallpaper.png
+set "base_url=https://raw.githubusercontent.com/oszuidwest/windows10-baseline/main/assets"
+set "wallpaper_path=C:\Windows\deploy\wallpaper.png"
+
+if "%_deploy_computer_purpose%"=="fm" (
+    curl -o "%wallpaper_path%" "%base_url%/ZWFM-wallpaper.png"
+)
+if "%_deploy_computer_purpose%"=="edit" (
+    curl -o "%wallpaper_path%" "%base_url%/ZWTV-wallpaper.png"
+)
+if "%_deploy_computer_purpose%"=="redactie" (
+    curl -o "%wallpaper_path%" "%base_url%/ZWUP-wallpaper.png"
+)
